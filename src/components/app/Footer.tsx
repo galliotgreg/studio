b221b186f42735e1065024ebfc46eb847dc6ae46
Grégory Galliot <greg@greg-ggt.com>
2025-08-25 20:04:02 +0000

    Ne peux tu pas simplement utiliser l’image que j’ai fourni, sans la modi

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -2,26 +2,8 @@
 "use client";
 
 import { Instagram, Linkedin } from "lucide-react";
+import Image from "next/image";
 import { useLanguage } from "./LanguageProvider";
-
-const BlueskyIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12 10c-3.5 0-7 2.5-7 7s3.5 7 7 7c3.5 0 7-2.5 7-7c0-4-3.5-7-7-7z" />
-      <path d="M12 10c-3.5 0-7-2.5-7-7s3.5-7 7-7c3.5 0 7 2.5 7 7c0 4-3.5 7-7-7z" />
-      <path d="M19 10c-3.5 0-7 2.5-7 7s3.5 7 7 7c3.5 0 7-2.5 7-7c0-4-3.5-7-7-7z" />
-      <path d="M5 10c-3.5 0-7 2.5-7 7s3.5 7 7 7c3.5 0 7-2.5 7-7c0-4-3.5-7-7-7z" />
-    </svg>
-);
 
 export function Footer() {
   const { t } = useLanguage();
@@ -29,7 +11,7 @@ export function Footer() {
   const socialLinks = [
     { name: "LinkedIn", icon: Linkedin, href: "#" },
     { name: "Instagram", icon: Instagram, href: "#" },
-    { name: "Bluesky", icon: BlueskyIcon, href: "#" },
+    { name: "Bluesky", icon: () => <Image src="/icons/bluesky.svg" alt="Bluesky" width={24} height={24} />, href: "#" },
   ];
 
   return (
