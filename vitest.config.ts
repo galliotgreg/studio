3357a63525251ee1585bf417d85a806d7c5c9e7d
Grégory Galliot <greg@greg-ggt.com>
2025-08-26 14:05:59 +0000

    FAIL  tests/gratitude.spec.ts [ tests/gratitude.spec.ts ] Error: Playwri

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './tests/unit/setup.ts',
+    exclude: ['node_modules', 'tests'],
   },
   resolve: {
     alias: {
