f7b91a02f0ec94170f218bd13324ec186fa1e6de
Grégory Galliot <greg@greg-ggt.com>
2025-08-30 13:21:25 +0000

    ça ne marche toujours pas, essayons quelque chose ne garde que modern et

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -11,22 +11,6 @@ export const THEMES: Theme[] = [
       id: 'default',
       nameKey: 'theme.modern',
     },
-    {
-      id: 'theme-lavande',
-      nameKey: 'theme.lavande',
-    },
-    {
-      id: 'theme-nature',
-      nameKey: 'theme.nature',
-    },
-    {
-      id: 'theme-ocean',
-      nameKey: 'theme.ocean',
-    },
-    {
-      id: 'theme-sunset',
-      nameKey: 'theme.sunset',
-    },
      {
       id: 'theme-grimoire',
       nameKey: 'theme.grimoire',
